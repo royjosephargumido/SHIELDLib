f5cf0b3c0f66cb8683c3e9e0aed86088878f5c1e
Roy Joseph Argumido <royjosephargumido@outlook.com>
2022-02-27 00:57:24 +0800

    Added testing sketch

--- src/test.ino
+++ src/test.ino
@@ -1,22 +1,68 @@
+#include <SD.h>
+#include <SPI.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+
+#define pin_csSD D8         //SD Card Chip Select Pin in NodeMCU
+#define slash '/'
+
+#define folder_Data "Data"
+#define folder_Audit "Audit"
+#define folder_Circadian "Circadian"
+#define folder_CIRRUS "CIRRUS"
+#define folder_Memories "Memories"
+
+#define folder_System "System"
+#define folder_Docu "Documentation"
+#define folder_Core "Core"
+
+#define fn_CoreConfiguration "BeaconConfig.shield"
+
+const String dir_audit = String() + folder_Data + slash + folder_Audit + slash;
+const String dir_circadian = String() + folder_Data + slash + folder_Circadian + slash;
+const String dir_cirrus = String() + folder_Data + slash + folder_CIRRUS + slash;
+const String dir_memories = String() + folder_Data + slash + folder_Memories + slash;
+const String dir_core = String() + folder_System + slash + folder_Core + slash + fn_CoreConfiguration;
+
+File file;
+StaticJsonDocument<192> rawdata_config;
+
 /**
- * @file test.ino
- * @author Roy Joseph B. Argumido (royjosephargumido@outlook.com)
- * @brief Main Arduino SHIELD Sketch
- * @version 0.1
- * @date 2022-02-10
- * 
- * @copyright Copyright (c) 2022 Team SHIELD
+ * @brief Types of SHIELD files 
  * 
  */
+enum FileToSave {
+    AUDIT_DATA,
+    CIRCADIAN_DATA,
+    CIRRUS_DATA,
+    TRANSCRIPT_DATA,
+    DUMP_DATA,
+    CONFIG_DATA
+};
 
-#include "SHIELDLib.h"
+void save(FileToSave _filetosave, String _rawdata) {
+    switch(_filetosave) {
+        case AUDIT_DATA:
+            break;
 
-Device device(BEACON);
+        case CIRCADIAN_DATA:
+            break;
+
+        case CIRRUS_DATA:
+            break;
+
+        case TRANSCRIPT_DATA:
+            break;
+
+        case DUMP_DATA:
+            break;
+
+        case CONFIG_DATA:
+            break;
+    }
+}
 
 void setup() {
-    Serial.begin(115200);
-
-    device.startDevice();
 }
 
 void loop() {
